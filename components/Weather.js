cd8ea15b135442a937809521c25763f19816634f
Thawatchai Jaihan <jh.thawatchai@gmail.com>
2022-08-27 17:38:25 +0700

    part 4

--- components/Weather.js
+++ components/Weather.js
@@ -25,4 +25,20 @@ const styles = StyleSheet.create({
     },
 });
 
-
+useEffect(() => {
+    console.log(`fetching data with zipCode = ${props.zipCode}`)
+    if (props.zipCode) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=...`)
+            .then((response) => response.json())
+            .then((json) => {
+                setForecastInfo({
+                    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                    temp: json.main.temp
+                });
+            })
+            .catch((error) => {
+                console.warn(error);
+            });
+    }
+}, [props.zipCode])
